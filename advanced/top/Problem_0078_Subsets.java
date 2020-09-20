b6c6d06f7648e58b6e074e48640827744dcf9c4c
andylight0528@gmail.com <andylight0528@gmail.com>
2020-09-20 13:18:51 +0100

    subset DFS

---
+++ advanced/top/Problem_0078_Subsets.java
@@ -0,0 +1,35 @@
+package advanced.top;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Problem_0078_Subsets {
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        LinkedList<Integer> path = new LinkedList<>();
+
+        process(nums, 0, path, res);
+        return res;
+    }
+
+    public static void process(int[] arr, int index, LinkedList<Integer> path, List<List<Integer>> res) {
+        if (index == arr.length) {
+            res.add(new ArrayList<>(path));
+        } else {
+            process(arr, index + 1, path, res); // 当前位置不统计进结果
+            path.addLast(arr[index]);
+            process(arr, index + 1, path, res); // 当前位置统计进结果
+            path.pollLast();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        System.out.println(subsets(nums));
+
+    }
+}
